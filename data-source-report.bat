29908a1c846f6fed56b87a0f71b4c307f5c610c2
Xitiz Bhatia <xitizbhatia@gmail.com>
2021-01-05 19:45:12 -0500

    Added support for JBoss authentication in datasource-report script

--- data-source-report.bat
+++ data-source-report.bat
@@ -7,11 +7,14 @@ rem NOTE: Set the value of these local variables
 set JBOSS_HOME=C:\java\jboss-eap-6.4
 set JBOSS_MANAGEMENT_IP=10.0.0.121
 set JBOSS_MANAGEMENT_PORT=9999
+set JBOSS_USER=
+set JBOSS_PASSWORD=
 set LOG_FILE=C:\java\jboss-eap-6.4\scripts\data-source-report.csv
 
 rem NOTE: DO NOT CHANGE ANYTHING BELOW THIS
 
 SET NOPAUSE=true & set str=
+set JBOSS_AUTH_STR=
 set DSNAME=DataSourceName
 set ACC=ActiveCount
 set AVC=AvailableCount
@@ -40,13 +43,25 @@ rem getting date time
 for /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
 for /f "tokens=1-2 delims=/:" %%a in ("%TIME%") do (set mytime=%%a%%b)
 
+rem setting up jboss auth
+if NOT "%JBOSS_USER%x" == "x" (
+  if NOT "%JBOSS_PASSWORD%x" == "x" (
+     set JBOSS_AUTH_STR=--user=%JBOSS_USER%^ --password=%JBOSS_PASSWORD%
+  ) else (
+     set JBOSS_AUTH_STR=
+  )  
+) else (
+set JBOSS_AUTH_STR=
+)
+
 rem now getting datasource statistics
 for %%G in (CONFIG_DS VT_DS EVENT_DS SECURITY_DS QRTZ_DS OVS_DS ATCH_DS) DO (
 (
 set CURRENT_DATASOURCE=%%G
 set str=%%G^,%mydate%_%mytime%
 set VALID_OUTPUT=
-for /f usebackq^ tokens^=2^,4^ delims^=^" %%A in (`%JBOSS_HOME%\bin\jboss-cli.bat "connect %JBOSS_MANAGEMENT_IP%:%JBOSS_MANAGEMENT_PORT%,/subsystem=datasources/data-source=!CURRENT_DATASOURCE!/statistics=pool:read-resource(recursive=true,include-runtime=true),exit"`) do (
+
+for /f usebackq^ tokens^=2^,4^ delims^=^" %%A in (`"%JBOSS_HOME%\bin\jboss-cli.bat --connect --controller=%JBOSS_MANAGEMENT_IP%:%JBOSS_MANAGEMENT_PORT% !JBOSS_AUTH_STR! /subsystem=datasources/data-source=!CURRENT_DATASOURCE!/statistics=pool:read-resource(recursive=true,include-runtime=true)"`) do (
     if "%%B" == "success" (
       set VALID_OUTPUT="Y"
     ) else (
@@ -74,5 +89,5 @@ for /f usebackq^ tokens^=2^,4^ delims^=^" %%A in (`%JBOSS_HOME%\bin\jboss-cli.ba
     )
    )
 )
-if NOT "!str!x" == "x" echo !str! >> %LOG_FILE%
+if NOT "!str!x" == "x" echo !str! >> %LOG_FILE% 2>&1
 )
